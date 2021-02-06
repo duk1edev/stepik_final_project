e53d5ac6c3684843e9baf7e6149045b336c4de3f
Vasiliy Pupkin <duk1e.drum@gmail.com>
2021-02-06 22:30:38 +0300

    import Login Page to test_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,10 @@
 from pages.main_page import MainPage # delete . for runtest
-
+from pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    # link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer '
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()
-    page.should_be_login_link()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
