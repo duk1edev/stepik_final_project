f14fd2ccfeeeec5e965f0adde86122e76f8a183a
Vasiliy Pupkin <duk1e.drum@gmail.com>
2021-02-06 21:14:14 +0300

    add is_element_present to BasePage

--- pages/main_page.py
+++ pages/main_page.py
@@ -7,3 +7,8 @@ class MainPage(BasePage):
         # login_link = self.browser.find_element_by_css_selector("#login_link")
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+
+    def should_be_login_link(self):
+        # assert self.browser.find_element(By.CSS_SELECTOR, "#login_link999"), 'Login link is not present'
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link333"), 'Login link is not present'
+
