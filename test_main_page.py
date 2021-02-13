93dfff624951b045db04b5c77bfac48620821067
Vasiliy Pupkin <duk1e.drum@gmail.com>
2021-02-14 00:27:19 +0300

    final fixing

--- test_main_page.py
+++ test_main_page.py
@@ -3,6 +3,14 @@ from pages.main_page import MainPage  # delete . for runtest
 from pages.login_page import LoginPage
 
 
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    page.should_be_empty_basket()
+
+
 @pytest.mark.login_guest
 class TestLoginFromMainPage():
     def test_guest_can_go_to_login_page(self, browser):
@@ -20,9 +28,4 @@ class TestLoginFromMainPage():
         page.should_be_login_link()
 
 
-def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_basket()
-    page.should_be_empty_basket()
+
--- test_product_page.py
+++ test_product_page.py
@@ -4,6 +4,32 @@ from pages.product_page import ProductPage  # delete . for runtest in WINDOWS
 from pages.login_page import LoginPage
 
 
+@pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.should_success_add_product_to_basket()
+
+
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    page.should_be_empty_basket()
+
+
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+
 class TestUserAddToBasketFromProductPage():
     @pytest.fixture(scope='function', autouse=True)
     def setup(self, browser):
@@ -29,29 +55,3 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.add_to_basket()
         page.should_success_add_product_to_basket()
-
-
-@pytest.mark.need_review
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    page = ProductPage(browser, link)
-    page.open()
-    page.add_to_basket()
-    page.should_success_add_product_to_basket()
-
-
-@pytest.mark.need_review
-def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_basket()
-    page.should_be_empty_basket()
-
-@pytest.mark.need_review
-def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-
